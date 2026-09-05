87f777c7cd1575d2cceb4d665c529eb79a086480
museh7 <m.hersi224@gmail.com>
2026-09-05 20:31:37 +0100

    Implemented the recursive step within the build_tree function

--- dectree.py
+++ dectree.py
@@ -17,13 +17,12 @@ def gini(y):
     return(1 - sum_squared_probs)
 x= gini(y)
 
+df1 = pd.read_csv("churn_data.csv")
 
-
-       
 
 # Test every feature and threshold, split churn labels left/right,
-def best_split():
-    df1 = pd.read_csv("churn_data.csv")
+def best_split(df1):
+    #remove el churn column
     df2 = df1.loc[:, df1.columns != 'churn']
     best_gini = 1 
     best_feature = "" 
@@ -43,20 +42,49 @@ def best_split():
             left_gini = gini(left_labels)
             right_gini = gini(right_labels)
             #weighted gini for this feature and threshold  and keep the split with the lowest score.
-            weighted_gini = (left_gini*(len(left_labels)/(len(left_labels)+len(right_labels)))+ right_gini*(len(right_labels)/(len(right_labels)+len(left_labels))))
-            if weighted_gini < best_gini :
+            weighted_gini = (left_gini * (len(left_labels) / (len(left_labels) + len(right_labels)))+ right_gini * (len(right_labels) / (len(right_labels) + len(left_labels))))
+            if weighted_gini < best_gini:
                 best_gini = weighted_gini 
                 best_feature = column 
                 best_threshold = threshold
+    return best_feature,best_threshold,best_gini
+# Creating a tree node class
+class TreeNode:
+    #decision nodes and leaf nodes, decisions won't have a prediction leaf's will
+    def __init__(self, feature, threshold, child_left, child_right, prediction):
+        self.feature = feature
+        self.threshold = threshold
+        self.child_left = child_left
+        self.child_right = child_right
+        self.prediction = prediction
 
-
-
-
-
-# STEP 6: CREATE A TREE NODE CLASS
-
-# STEP 7: WRITE build_tree() USING RECURSION
-
+#build_tree function using recusrsion
+def build_tree(df1):
+    #check for stopping conditions
+    churn_list = df1['churn']
+    #subset already pure?
+    if gini(churn_list) == 0:
+        return TreeNode(None, None, None, None, churn_list.iloc[0]) #returns prediction as first element of subset if pure alr
+    x = best_split(df1)
+    # No valid split found
+    if x[0] == "":
+        #predictes label with higher count
+        prediction = churn_list.value_counts().idxmax()
+        return TreeNode(None, None, None, None, prediction)
+    #find weighted gini and compare to gini(churn_list)
+    split_gini = x[2]
+    #second stopping condition are splites improving gini purity?
+    if split_gini >= gini(churn_list):
+        prediction = churn_list.value_counts().idxmax()
+        return TreeNode(None, None, None, None, prediction)
+    #make the dataframe smaller by using best thresholds 
+    left_subset = df1[df1[x[0]] < x[1]]
+    right_subset = df1[df1[x[0]] >= x[1]]
+    #recursive step keep calling build_tree until tree is done
+    child_left = build_tree(left_subset)
+    child_right = build_tree(right_subset)
+    return TreeNode(x[0], x[1], child_left, child_right, None)
+    
 # STEP 8: WRITE predict_one()
 
 # STEP 9: WRITE predict() FOR MANY ROWS
--- test_gini.py
+++ test_gini.py
@@ -1,4 +1,5 @@
 from dectree import gini
+import pandas as pd
 print(gini([1, 1, 1, 1, 1]))
 print(gini([7, 7, 7, 7]))
 print(gini([0, 1]))
@@ -8,3 +9,5 @@ print(gini([0, 1, 2]))
 print(gini([0, 0, 0, 1, 2]))
 print(gini(["cat", "dog", "dog", "cat"]))
 print(gini(["A", "B", "C", "D"]))
+
+
